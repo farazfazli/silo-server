7ec507e1d37f153754305fe88ea9022e06bdca60
Faraz Fazli <farazfazli@gmail.com>
2016-10-23 03:42:31 -0400

    Update makeaci.sh

--- makeaci.sh
+++ makeaci.sh
@@ -3,6 +3,7 @@ alias acbuild="/home/faraz/acbuild"
 acbuild begin
 trap "{ export EXT=$?; acbuild --debug end && exit $EXT;   }" EXIT
 acbuild set-name server
+acbuild --debug copy-to-dir static /srv/static
 acbuild --debug copy-to-dir server /srv
 acbuild --debug set-exec /srv/server
 acbuild label add version 0.0.1
